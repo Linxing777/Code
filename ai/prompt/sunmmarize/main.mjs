a391188edc0b45017e2916e0f4c86acaeaa1d3dd
pys <1047759518@qq.com>
2025-03-11 16:10:24 +0800

    AI

--- ai/prompt/sunmmarize/main.mjs
+++ ai/prompt/sunmmarize/main.mjs
@@ -1,21 +1,28 @@
 import OpenAI  from "openai";
 import dotenv from "dotenv";
-console.log(process.env)
-// const clien = new OpenAI({
-//   apiKey: "sk-641292945533452784",
-//   baseURL:""
-// });
+dotenv.config();
+const clien = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+  baseURL: process.env.OPENAI_BASE_URL
+});
 
-// const response = await clien.chat.completions.create({
-//   model: "gpt-3.5-turbo",
-//   messages: [
-//     {
-//       role: "system",
-//       content: "You are a helpful assistant."
-//     },
-//     {
-//       role: "user",
-//       content: "Hello!"
-//     }
-//   ]
-// });
+//es6 默认参数值
+//通用LLM 聊天完成接口函数 复用
+const getCompletion = async (prompt, model = "gpt-3.5-turbo") => {
+  const messages = [{ role: "user", content: prompt }];
+  // AIGC chat 接口
+  const response = await clien.chat.completions.create({
+    model: model,
+    messages: messages,
+    temperature: 0, // 模型输出的随机性
+  });
+  return response.choices[0].message.content;
+}
+
+
+const main = async () => {
+  const prod_review = ``;
+  const prompt = `'''${prompt}'''`;
+  const response = await getCompletion(prompt,'gpt-3.5-turbo');
+  console.log(response);
+}
